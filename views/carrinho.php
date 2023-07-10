f72fa92161f677d6a1df761d992af23cbcabe73d
Dnmaster-Dev <estevaothatymartins2325@gmail.com>
2023-07-10 20:48:32 -0300

    login

---
+++ views/carrinho.php
@@ -0,0 +1,10 @@
+<?php 
+require_once $_SERVER["DOCUMENT_ROOT"] . "/Carrinho/templates/cabecalho.php";
+?>
+
+
+
+
+<?php 
+require_once $_SERVER["DOCUMENT_ROOT"] . "/Carrinho/templates/rodape.php";
+?>
--- views/login.php
+++ views/login.php
@@ -7,20 +7,14 @@ require_once $_SERVER["DOCUMENT_ROOT"] . "/Carrinho/templates/cabecalho.php";
             <fieldset id="loginmod">
                 <h1 id="h1style">Login</h1>
                 <p class= "fontelogin">Digite os seus dados de acesso nos campos abaixo.</p> 
-                <br>
                 <div>
                     <input type="mail" class="bordainput meu-input" name="nome" id="nome" placeholder="E-mail" required autofocus>
                 </div>
-                <br>
                 <div>
                     <input type="password" class="bordainput meu-input" name="senha" id="senha" placeholder="Senha" required>
                 </div>
-                <br>
                 <button type="submit" id="buttonb">Acessar</button>
-                <br><br>
-                <p><a href="esqueci_senha.html" id="linkb">Esqueci minha senha</a></p> 
-                <p><a href="/Carrinho/views/cadastro_usuario.php"  id="linkb">Cadastrar</a></p> 
-                <br>
+                <p><a href="/Carrinho/views/cadastro_usuario.php"  id="linkb" style="margin-top: 10px;">Cadastrar</a></p> 
 
             </fieldset>
          </form>
