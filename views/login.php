27eda55273e91a7ef25fbe9c9e5fd3979e5bacfe
André Santos <af4felipedutra@gmail.com>
2023-07-11 17:10:19 -0300

    login corrigido

--- views/login.php
+++ views/login.php
@@ -6,15 +6,15 @@ require_once $_SERVER["DOCUMENT_ROOT"] . "/Carrinho/templates/cabecalho.php";
         <form action="" autocomplete="off" method="POST">
             <fieldset id="loginmod">
                 <h1 id="h1style">Login</h1>
-                <p class= "fontelogin">Digite os seus dados de acesso nos campos abaixo.</p> 
-                <div>
+                <p class= "fontelogin" style="margin-bottom: 10px;">Digite os seus dados de acesso nos campos abaixo.</p> 
+                <div style="margin-bottom: 10px;">
                     <input type="mail" class="bordainput meu-input" name="nome" id="nome" placeholder="E-mail" required autofocus>
                 </div>
-                <div>
+                <div style="margin-bottom: 10px;">
                     <input type="password" class="bordainput meu-input" name="senha" id="senha" placeholder="Senha" required>
                 </div>
                 <button type="submit" id="buttonb">Acessar</button>
-                <p><a href="/Carrinho/views/cadastro_usuario.php"  id="linkb" style="margin-top: 10px;">Cadastrar</a></p> 
+                <p><a href="/Carrinho/views/cadastro_usuario.php"  id="linkb" style="margin-top: 5px;">Cadastrar</a></p> 
 
             </fieldset>
          </form>
